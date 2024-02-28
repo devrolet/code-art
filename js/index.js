0cf99d1b7d2afc81d7edc7e8248b1718e6f9d0ee
Chet Hill <chetdhill@gmail.com>
2024-02-28 08:33:39 -0600

    Create Atom class, properties, and methods

--- js/index.js
+++ js/index.js
@@ -3,36 +3,24 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// console.log(ctx);
+class Atom {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = Math.random() * 8 + 2;
+        this.speedX = Math.random() * 4 - 2; //-2 +2
+        this.speedY = Math.random() * 4 - 2; //-2 +2
+    }
 
-// Creates a rectangle on every mouse click
-// canvas.addEventListener('click', (e) => {
-//     ctx.beginPath();
-//     ctx.rect(e.x, e.y, 10, 10);
-//     ctx.fill();
-// });
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
 
-// Creates a rect as mouse moves, in essense a pen tool
-// canvas.addEventListener('mousemove', (e) => {
-//     ctx.beginPath();
-//     ctx.rect(e.x, e.y, 10, 10);
-//     ctx.fill();
-// });
-// Degrees to Radians formula and function
-let degToRad = (deg) => (deg / 180) * Math.PI;
-
-ctx.beginPath();
-// ctx.arc(200, 200, 100, 0, Math.PI * 2); // creates circle
-// ctx.arc(200, 200, 100, 0, Math.PI); // creates half circle (bottom half)
-// ctx.arc(200, 200, 100, Math.PI, Math.PI * 2); // creates half circle (top half);
-ctx.arc(200, 200, 100, 0, degToRad(360)); // creates circle using a function as a param
-// ctx.arc(200, 200, 100, degToRad(270), degToRad(360)); // quarter circle
-ctx.stroke();
-ctx.fill();
-
-// Degrees to Radians (long approach)
-// let degreesToRadians = (degrees) => {
-//     let pi = Math.pi;
-//     return degrees * (pi/180);
-// }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
 
