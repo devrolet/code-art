9cbf8448342c39cd0307cdef0497e8d02bea2ee7
Chet Hill <chetdhill@gmail.com>
2024-02-28 10:20:45 -0600

    Moving particles parabolically

--- js/index.js
+++ js/index.js
@@ -58,16 +58,19 @@ class Atom {
         ctx.fill();
     }
 }
+
 const point = {
-    x: 700,
-    y: 100
+    x: 0,
+    y: 0
 }
 
+let degree = 0;
 const generateAtoms = () => {
-    atoms.push(new Atom(
-        point.x,
-        point.y
-    ));
+    atoms.push(new Atom(canvas.width / 2 + (point.x * 200), canvas.height / 2 + (point.y * 200)));
+    point.x = Math.cos(degree / 180 * Math.PI);
+    point.y = point.x * point.x;
+
+    degree++;
     requestAnimationFrame(generateAtoms);
 }
 
