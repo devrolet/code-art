1c3b8ac5575fd76ff0bdb318844f8fe6d3b00192
Chet Hill <chetdhill@gmail.com>
2024-02-28 10:55:37 -0600

    Complete Chaos Game/Pattern

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -2,3 +2,56 @@ const canvas = document.getElementById("my-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+
+class Point {
+    constructor(x, y) {
+        this.x = x,
+        this.y = y,
+        this.radius = 1;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+// Triangle Top
+const triangleTop = new Point(window.innerWidth / 2, 50);
+triangleTop.draw();
+
+const triangleBottomLeft = new Point(50, window.innerHeight - 50);
+triangleBottomLeft.draw();
+
+const triangleBottomRight = new Point(window.innerWidth - 50, window.innerHeight - 50);
+triangleBottomRight.draw();
+
+const triangle = [triangleTop, triangleBottomLeft, triangleBottomRight];
+
+let getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+let drawRest = (prevPoint) => {
+    let randomCorner, middlePoint;
+
+    for (let i = 0; i < 100000; i++) {
+        // pick random corner
+        randomCorner = triangle[getRandomInt(0, 3)];
+        // find middle point
+        middlePoint = new Point(
+            (prevPoint.x + randomCorner.x) / 2,
+            (prevPoint.y + randomCorner.y) / 2
+        );
+        middlePoint.draw();
+        prevPoint = middlePoint;
+    }
+}
+
+canvas.addEventListener('click', (e) => {
+    const firstPoint = new Point(e.x, e.y);
+    firstPoint.draw();
+    drawRest(firstPoint);
+},
+{once: true}
+)
