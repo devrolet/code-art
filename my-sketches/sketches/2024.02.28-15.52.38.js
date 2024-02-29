5569d2d693d8c7708c36515b204d7b90a1015be8
Chet Hill <chetdhill@gmail.com>
2024-02-29 08:05:25 -0600

    Practice Rectangles

--- my-sketches/sketches/2024.02.28-15.52.38.js
+++ my-sketches/sketches/2024.02.28-15.52.38.js
@@ -1,9 +1,9 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  // dimensions: [ 2048, 2048 ]
-  dimensions: "A4",
-  orientation: "landscape"
+  dimensions: [ 2048, 2048 ]
+  // dimensions: "A4",
+  // orientation: "landscape"
 };
 
 const sketch = () => {
@@ -11,10 +11,17 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
+    const rectWidth = width * 0.02;
+    const rectHeight = height * 0.2;
+
     context.fillStyle = 'black';
-    context.beginPath();
-    context.rect(100, 100, 100, 100);
-    context.stroke();
+    
+    // Create 12 rectangles
+    for (let i = 0; i < 12; i++) {
+      context.beginPath();
+      context.rect(100 * i, 100, rectWidth, rectHeight);
+      context.fill();
+    }
   };
 };
 
