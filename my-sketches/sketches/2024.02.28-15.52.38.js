24926903d479ccffc6907e9d04f868c90c56c135
Chet Hill <chetdhill@gmail.com>
2024-02-29 07:58:04 -0600

    Create Rectangle

--- my-sketches/sketches/2024.02.28-15.52.38.js
+++ my-sketches/sketches/2024.02.28-15.52.38.js
@@ -1,13 +1,20 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  // dimensions: [ 2048, 2048 ]
+  dimensions: "A4",
+  orientation: "landscape"
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.rect(100, 100, 100, 100);
+    context.stroke();
   };
 };
 
